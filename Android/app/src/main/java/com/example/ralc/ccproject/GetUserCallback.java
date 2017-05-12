8465c64bf4b47d78da3af6578422fc31eaead3bd
Yidong47 <yidong_ren@outllook.com>
2017-05-12 13:21:47 -0400

    Add Android

---
+++ Android/app/src/main/java/com/example/ralc/ccproject/GetUserCallback.java
@@ -0,0 +1,12 @@
+package com.example.ralc.ccproject;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by ralc on 5/9/17.
+ */
+
+public interface GetUserCallback {
+    public abstract void flagged(boolean flag);
+    public abstract void imgData(String x);
+}
